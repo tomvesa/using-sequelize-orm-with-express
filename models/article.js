af5258ffb4978d6d0107da80ca4bddf7d658e75c
tomvesa <47905734+tomvesa@users.noreply.github.com>
2023-09-27 00:37:53 +0200

    commit 1

---
+++ models/article.js
@@ -0,0 +1,41 @@
+'use strict';
+const Sequelize = require('sequelize');
+const moment = require('moment');
+
+module.exports = (sequelize) => {
+    class Article extends Sequelize.Model{
+        publishedAt() {
+            const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
+            return date;
+        }
+        shortDescription() {
+            const shortDesc = this.body.length > 200 
+                                ? this.body.substring(0, 200) + "..." 
+                                : this.body;
+            return shortDesc;
+        }
+    }
+    
+    Article.init({
+        title: {
+            type: Sequelize.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Please provide title"
+                }     
+            }
+        },
+        author: {
+            type: Sequelize.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Athor name required"
+                }
+            } 
+        },
+        body: Sequelize.TEXT,
+        
+    }, { sequelize });
+
+    return Article;
+};
